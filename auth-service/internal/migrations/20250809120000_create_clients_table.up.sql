f33a8fa58a8d420f9ef2c62d903a84364b1229b8
Harsh Bhama <harshbhama96@gmail.com>
2026-01-14 15:48:23 +0530

    add client agency relationship

--- auth-service/internal/migrations/20250809120000_create_clients_table.up.sql
+++ auth-service/internal/migrations/20250809120000_create_clients_table.up.sql
@@ -186,6 +186,19 @@ ALTER TABLE agency
   DROP CONSTRAINT agency_location_check;
 
 
+
+CREATE TABLE client_agencies (
+    client_id BIGINT NOT NULL,
+    agency_id INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_client_agency FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE,
+    CONSTRAINT fk_agency FOREIGN KEY (agency_id) REFERENCES agency(id) ON DELETE CASCADE,
+
+    CONSTRAINT pk_client_agency PRIMARY KEY (client_id, agency_id)
+);
+
+
 ALTER TABLE projects
 ADD COLUMN category TEXT,
 ADD COLUMN priority TEXT,
